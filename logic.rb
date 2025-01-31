a8101c35af850bf5911bc789570f7c56454591de
La Tasha L.  Pollard <128096377+LilLoveBytes@users.noreply.github.com>
2025-01-31 17:40:21 +0000

    updates some poke api calls

--- logic.rb
+++ logic.rb
@@ -6,10 +6,21 @@ require "json"
 random_id = rand(1..1025)
 
 # logic to get name of pokemon  
-poke_url = "https://pokeapi.co/api/v2/pokemon/" + "#{random_id}"
+poke_url = "https://pokeapi.co/api/v2/pokemon/" + "71"
 results = HTTP.get(poke_url)
 parsed_results = JSON.parse(results)
 poke_name = parsed_results["forms"][0]["name"]
 pp poke_name
 
 # logic to get pokemon info
+
+# height in decimeters
+poke_height = parsed_results["height"]
+# weight in hectograms
+poke_weight = parsed_results["weight"]
+# type
+poke_types = parsed_results["types"].map { |i| i["type"]["name"] }
+# moves
+num_moves = parsed_results["moves"].count
+moves_list = parsed_results["moves"].map { |i| i["move"]["name"]}
+random_move = moves_list.sample
