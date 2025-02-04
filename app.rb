268d4f6cfa61d5aa6db10825ba8ae93f29268deb
La Tasha L.  Pollard <128096377+LilLoveBytes@users.noreply.github.com>
2025-02-04 10:58:37 +0000

    adds bind and port variables

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require "http"
 require "json"
 
 set :public_folder, File.dirname(__FILE__) + '/public'
+set :bind, '0.0.0.0'
+set :port, ENV['PORT'] || 10000 # Optionally set a custom port
+
 
 get("/") do
   "
